37f7676f8982c62d8db2900d1fd8b294cbd48ff6
Alexander Neumann <alexander@bumpern.de>
2009-11-22 16:44:48 +0100

    split start_program.rb for colorwheel and random

--- tests/start_program_random.rb
+++ tests/start_program_random.rb
@@ -14,12 +14,5 @@ sync()
 puts "stop fading"
 stop(255)
 
-puts "start program colorwheel"
-start_program(255, 0, [5, 1, 10,  0, 0,  60, 0, 255, 255])
-
-sleep 4
-puts "stop"
-stop(255)
-
-puts "fade to hsv color"
-fade_hsv(255, 300, 180, 100, 2, 1)
+puts "start program random"
+start_program(255, 1, [0, 0, 0, Time.now.usec & 0xff, 1,   5, 1, 10,  255, 255,  40])
