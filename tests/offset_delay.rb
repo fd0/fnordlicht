ce34299109b648439435bbf8598cb0c1f1aac46d
Alexander Neumann <alexander@bumpern.de>
2009-11-22 18:21:18 +0100

    bugfix test-scripts: default offset for val/sat is 255

--- tests/offset_delay.rb
+++ tests/offset_delay.rb
@@ -15,7 +15,7 @@ puts "stop fading"
 stop(255)
 fade_rgb(255,  0, 0, 0,  255, 0)
 puts "reset offsets "
-config_offsets(255,  0, 0,   0, 0, 0)
+config_offsets(255,  0, 0,   0, 255, 255)
 
 puts "fade slow rgb (big steps)"
 fade_updown_rgb(255,  255, 255, 0,  10, 10,  3)
@@ -24,7 +24,7 @@ puts "fade slow hsv (big steps)"
 fade_updown_hsv(255,  120, 255, 255,  10, 10,  3)
 
 puts "configuring delay offset"
-config_offsets(255,  0, -5,   0, 0, 0)
+config_offsets(255,  0, -5,   0, 255, 255)
 
 puts "fade slow rgb (big steps, but modified to fast)"
 fade_updown_rgb(255,  255, 255, 0,  10, 10,  1)
--- tests/offset_step.rb
+++ tests/offset_step.rb
@@ -15,7 +15,7 @@ puts "stop fading"
 stop(255)
 fade_rgb(255,  0, 0, 0,  255, 0)
 puts "reset offsets "
-config_offsets(255,  0, 0,   0, 0, 0)
+config_offsets(255,  0, 0,   0, 255, 255)
 
 puts "fade slow rgb"
 fade_updown_rgb(255,  255, 255, 0,  1, 1,  3)
@@ -24,7 +24,7 @@ puts "fade slow hsv"
 fade_updown_hsv(255,  120, 255, 255,  1, 1,  3)
 
 puts "configuring positive step offset"
-config_offsets(255,  10, 0,   0, 0, 0)
+config_offsets(255,  10, 0,   0, 255, 255)
 
 puts "fade slow rgb (but modified to fast)"
 fade_updown_rgb(255,  255, 255, 0,  1, 1,  1)
@@ -33,7 +33,7 @@ puts "fade slow hsv (but modified to fast)"
 fade_updown_hsv(255,  120, 255, 255,  1, 1,  1)
 
 puts "reset offsets "
-config_offsets(255,  0, 0,   0, 0, 0)
+config_offsets(255,  0, 0,   0, 255, 255)
 
 puts "fade fast rgb"
 fade_updown_rgb(255,  255, 255, 0,  10, 1,  1)
@@ -42,7 +42,7 @@ puts "fade fast hsv"
 fade_updown_hsv(255,  120, 255, 255,  10, 1,  1)
 
 puts "configuring negative step offset"
-config_offsets(255,  -10, 0,   0, 0, 0)
+config_offsets(255,  -10, 0,   0, 255, 255)
 
 puts "fade fast rgb (but modified to slow)"
 fade_updown_rgb(255,  255, 255, 0,  10, 1,  3)
