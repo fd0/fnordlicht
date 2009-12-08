6aa3c55ffd3b3465e4cad0dd7bcc9b9f1e23fe8c
Alexander Neumann <alexander@bumpern.de>
2009-12-08 01:28:25 +0100

    pwm: initialize sorted[] static

--- fnordlicht-firmware/pwm.c
+++ fnordlicht-firmware/pwm.c
@@ -220,9 +220,7 @@ void pwm_poll_fading(void)
 /** update pwm timeslot table (for target color) */
 void update_pwm_timeslots(struct rgb_color_t *target)
 {
-    static uint8_t sorted[PWM_CHANNELS];
-    for (uint8_t i = 0; i < PWM_CHANNELS; i++)
-        sorted[i] = i;
+    static uint8_t sorted[PWM_CHANNELS] = {0, 1, 2};
 
     /* sort channels according to the current brightness */
     for (uint8_t i = 0; i < PWM_CHANNELS; i++) {
