4aece731f5eede35627e16b827e9f633b112141e
Alexander Neumann <alexander@bumpern.de>
2009-12-12 20:04:12 +0100

    use address given on the commandline

--- tests/enter_application.rb
+++ tests/enter_application.rb
@@ -9,8 +9,13 @@ include Fnordlicht
 
 $dev = SerialPort.new("/dev/ttyUSB0", 19200)
 
+if ARGV.length != 1
+    $stderr.puts "USAGE: #{$0} <addr>"
+    exit 1
+end
+
 puts "sending sync sequence"
 sync()
 
 puts "starting application"
-boot_enter_application(255)
+boot_enter_application(ARGV.shift.to_i)
